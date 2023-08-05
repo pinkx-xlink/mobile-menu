f1c32b57b6aa343bc73d752767b83113779aae2e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-05 12:43:09 -0400

    Hide tabs until clicked on!

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function selectTab(tabIndex) {
     //hide tabs
-    document.getElementById("")
-    document.getElementById("")
-    document.getElementById("")
-    document.getElementById("")
+    document.getElementById("tab1Content").style.display = "none";
+    document.getElementById("tab2Content").style.display = "none";
+    document.getElementById("tab3Content").style.display = "none";
+    document.getElementById("tab4Content").style.display = "none";
     //show selected tab
     document.getElementById("tab" + tabIndex + "Content").style.display = "block";
 }
