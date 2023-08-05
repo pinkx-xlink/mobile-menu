b3d2bb0780a49df9d1d1f083134848ce59d72eb6
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-05 12:32:40 -0400

    Add frame for JS to select/hide tabs

---
+++ app.js
@@ -0,0 +1,9 @@
+function selectTab(tabIndex) {
+    //hide tabs
+    document.getElementById("")
+    document.getElementById("")
+    document.getElementById("")
+    document.getElementById("")
+    //show selected tab
+    document.getElementById("tab" + tabIndex + "Content").style.display = "block";
+}
